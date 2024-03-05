9319d3f632fe369a4717ca0373cd220e1998e1b1
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-05 19:47:16 +0000

    s3

--- src/components/App.js
+++ src/components/App.js
@@ -13,11 +13,11 @@ function input(){
 
   return (
     <div>
-      <h1>Select your gender:</h1>
-         <label for="r1">HTML :</label>
+      <h2>Select your gender:</h2>
          <input type="radio" id="r1" name="gender" value="male" onClick={input}/>
-         <label for="r2">CSS :</label>
+         <label for="r1">Male</label>
          <input type="radio" id="r2" name="gender" value="female" onClick={input}/>
+         <label for="r2">Female</label>
          
         {
           val==="male"?(
@@ -25,7 +25,7 @@ function input(){
           <h2>Select your shirt size:</h2>
       
           <select name="size" id="size">
-  <option value="">select size</option>
+  <option >select size</option>
   <option value="Small">Small</option>
   <option value="Medium">Medium</option>
   <option value="Large">Large</option>
@@ -34,7 +34,7 @@ function input(){
           ):(<div>
           <h2>Select your dress size:</h2>
           <select name="size" id="size">
-  <option value="">select size</option>
+  <option >select size</option>
   <option value="2">2</option>
   <option value="4">4</option>
   <option value="6">6</option>
